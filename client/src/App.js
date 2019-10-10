b4e6bc83191cbfaf130b5ff4dbc37bba11681a18
Philip Vieira <philipvieira00@gmail.com>
2019-10-10 19:25:24 -0400

    Began adding real functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import API from "../src/utils/API";
+import {Container, Row, Col} from "../src/Components/Grid";
+import {IceCreamList, IceCreamListItem} from "../src/Components/IceCreamList"
+import { Input, TextArea, FormBtn } from "../components/Form";
+import { List, ListItem } from "../components/List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state ={
+    iceCreams: [],
+    iceCreamSearch: ""
+  };
+
+  handleInputChange = event => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    API.getProduct(this.state.iceCreamSearch)
+    .then(res => this.setState({iceCreams: res.data}))
+    .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav/>
+        <form>
+        <Container>
+          <Row>
+            <Col size="xs-9  sm-10">
+              <Input 
+              name="iceCreamSearch"
+              value={this.state.iceCreamSearch}
+              onChange={this.handleInputChange}
+              placeholder= "Search for a specific ice cream"
+              />
+            </Col>
+            <Button 
+            onClick={this.handleFormSubmit}
+            type="success"
+            className="input-lg"
+            >
+              Search
+            </Button>
+          </Row>
+        </Container>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
